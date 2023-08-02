9f96eb743bfd42a79c6800bf6f089c70946f0ea4
RPelss <pelss.rudis@gmail.com>
2023-08-02 15:51:08 +0300

    Added puzzle string parsing to 2D array

--- lib/validator.rb
+++ lib/validator.rb
@@ -9,5 +9,25 @@ class Validator
 
   def validate
     # Your code here
+
+    # parse puzzle string to 9x9 int array
+    puzzle = extract_numbers(@puzzle_string)
+
+    # check for invalid numbers
+    # check each row
+    # check each column
+    # check each square
+    return puzzle
+  end
+
+  def extract_numbers (from)
+
+    result = from.scan(/\d*/)                 # getting all the numbers out of the string with Regex
+    result.delete("")                                   # removing empty strings
+    raise "Sudoku has missing numbers!" unless result.count == 81
+    result = result.each_slice(9).to_a        # transforming to a 9x9 array
+    result.map! { |row| row.map! { |tile| tile.to_i} }  # casting to integers
+
+    return result
   end
 end
